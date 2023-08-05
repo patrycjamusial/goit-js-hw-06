df0b9b0228b84a001cda7b85b3a9022fa7c37c24
Patrycja Musiał <patrycjamusial1@gmail.com>
2023-08-05 13:31:24 +0200

    added task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsAdd = document.querySelector("#ingredients");
+
+const itemsAdd = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+ingredientsAdd.append(...itemsAdd);
