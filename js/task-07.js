e5ccc43a8c5048983c85bf6352e2f86cd3671b39
Patrycja Musiał <patrycjamusial1@gmail.com>
2023-08-13 17:01:03 +0200

    changed task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,9 @@
 const fontSizeControl = document.getElementById("font-size-control");
 const textSpan = document.getElementById("text");
+fontSizeControl.setAttribute("value", "16");
 
 function changeFontSize() {
   const fontSizeValue = fontSizeControl.value;
   textSpan.style.fontSize = `${fontSizeValue}px`;
 }
-
 fontSizeControl.addEventListener("input", changeFontSize);
